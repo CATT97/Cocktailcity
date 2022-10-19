20317e3c97361f40fdfad422cbba9151b10140f9
Alexis Tabares <cesar.tabares26@hotmail.com>
2022-10-19 17:21:20 -0500

    index compras

--- resources/views/comprausuario/index.blade.php
+++ resources/views/comprausuario/index.blade.php
@@ -1,1 +1,31 @@
-hola {{ $compras }}
+@extends('layouts.app')
+
+@section('content')
+
+<h1 class="text-center">Lista de compras</h1>
+<hr>
+
+<table class="container table table-dark table-hover text-center">
+    <thead class="table-light">
+        <tr>
+            <th>Fecha</th>
+            <th>Estado</th>
+            <th>Valor</th>
+            <th>medio</th>
+            <th>Opciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($compras as $item)
+            <tr>
+                <td>{{ $item->FechaHora}}</td>
+                <td>{{ $item->Estado }}</td>
+                <td>{{ $item->ValorTotal }}</td>
+                <td>{{ $item->MedioCompra }}</td>
+                <td><a href="{{ route('compras.show', $item) }}" class="btn btn-warning">Ver Detalles</a></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
